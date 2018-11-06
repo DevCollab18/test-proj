c1efc5d1e239fd0703fe0c06c55e41abc46d2319
ca6.justin <ca6.justin@enfrausaph.com>
2018-11-07 06:22:58 +0800

    Changed to Bootstrap

--- public/client.js
+++ public/client.js
@@ -3,7 +3,7 @@ $(function(){
     $("#navbar").load("partials/navbar.html"); 
     });
 
-    //Autocomplete Datalist
+//Autocomplete Datalist
 var selected;
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
@@ -39,21 +39,50 @@ $(document).ready(function() {
                     var myObj = JSON.parse(this.responseText);
                     for(x in myObj){
                         if (selected == myObj[x].client ){
-                            $('#client').html(jQuery.parseHTML(myObj[x].client));
-                            $('#job_owner').html(jQuery.parseHTML(myObj[x].job_owner));
-                            $('#trackingnumber').html(jQuery.parseHTML(myObj[x].tracking_number));
-                            $('#need_acceptance').html(jQuery.parseHTML(myObj[x].need_acceptance));
-                            $('#accept_phone').html(jQuery.parseHTML(myObj[x].ok_to_accept_srs_over_the_phone));
-                            $('#trades').html(jQuery.parseHTML(myObj[x].trades));
-                            $('#nte').html(jQuery.parseHTML(myObj[x].nte));
-                            $('#ok_to_sub').html(jQuery.parseHTML(myObj[x].ok_to_sub));
-                            $('#cam').html(jQuery.parseHTML(myObj[x].cam));
-                            $('#cc').html(jQuery.parseHTML(myObj[x].cc));
-                            $('#website').html(jQuery.parseHTML(myObj[x].website));
-                            $('#instructions').html(jQuery.parseHTML(myObj[x].special_instructions));
-                            
+                            CKEDITOR.disableAutoInline = true;
+                            $('#client, #client2').html(jQuery.parseHTML(myObj[x].client));
+                            $('#job_owner, #job_owner2').html(jQuery.parseHTML(myObj[x].job_owner));
+                            $('#trackingnumber, #trackingnumber2').html(jQuery.parseHTML(myObj[x].tracking_number));
+                            $('#need_acceptance, #need_acceptance2').html(jQuery.parseHTML(myObj[x].need_acceptance));
+                            $('#accept_phone, #accept_phone2').html(jQuery.parseHTML(myObj[x].ok_to_accept_srs_over_the_phone));
+                            $('#trades, #trades2').html(jQuery.parseHTML(myObj[x].trades));
+                            $('#nte, #nte2').html(jQuery.parseHTML(myObj[x].nte));
+                            $('#ok_to_sub, #ok_to_sub2').html(jQuery.parseHTML(myObj[x].ok_to_sub));
+                            $('#cam, #cam2').html(jQuery.parseHTML(myObj[x].cam));
+                            $('#cc, #cc2').html(jQuery.parseHTML(myObj[x].cc));
+                            $('#website, #website2').html(jQuery.parseHTML(myObj[x].website));
+                            $('#instructions, #instructions2').html(jQuery.parseHTML(myObj[x].special_instructions));
+
+                            $("#client2,#client2 + div").html($('#client2').html());
+                            $("#job_owner2,#job_owner2 + div").html($('#job_owner2').html());
+                            $("#trackingnumber2,#trackingnumber2 + div").html($('#trackingnumber2').html());
+                            $("#need_acceptance2,#need_acceptance2 + div").html($('#need_acceptance2').html());
+                            $("#accept_phone2,#accept_phone2 + div").html($('#accept_phone2').html());
+                            $("#trades2,#trades2 + div").html($('#trades2').html());
+                            $("#nte2,#nte2 + div").html($('#nte2').html());
+                            $("#ok_to_sub2,#ok_to_sub2 + div").html($('#ok_to_sub2').html());
+                            $("#cam2,#cam2 + div").html($('#cam2').html());
+                            $("#cc2,#cc2 + div").html($('#cc2').html());
+                            $("#website2,#website2 + div").html($('#website2').html());
+                            $("#instructions2,#instructions2 + div").html($('#instructions2').html());
+
+                            $("#client, #job_owner, #trackingnumber, #need_acceptance, #accept_phone, #trades, #nte, #ok_to_sub, #cam, #cc, #website, #instructions").css("display","block");
+                            $("#client2, #job_owner2, #trackingnumber2, #need_acceptance2, #accept_phone2, #trades2, #nte2, #ok_to_sub2, #cam2, #cc2, #website2, #instructions2, #client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").css("display","none"); 
+
+                            $('.search-input').val("");
+                            CKEDITOR.inline('client2');
+                            CKEDITOR.inline('jobowner2');
+                            CKEDITOR.inline('trackingnumber2');
+                            CKEDITOR.inline('acceptance2');
+                            CKEDITOR.inline('accept-sr2');
+                            CKEDITOR.inline('trades2');
+                            CKEDITOR.inline('nte2');
+                            CKEDITOR.inline('sub2');
+                            CKEDITOR.inline('cam2');
+                            CKEDITOR.inline('cc2');
+                            CKEDITOR.inline('website2');
+                            CKEDITOR.inline('instructions2');
                         }
-                        $('.search-input').val("");
                     }
                 }
             };
@@ -63,26 +92,33 @@ $(document).ready(function() {
     })
 });
 
-// Adding & Creating Client Function
-$(document).ready(function(){ 
-    $("#update-btn,#update-btn2").attr('disabled', true);
+// Updating & Creating Client Function
+$(document).ready(function(){
+    $("#client2, #job_owner2, #trackingnumber2, #need_acceptance2, #accept_phone2, #trades2, #nte2, #ok_to_sub2, #cam2, #cc2, #website2, #instructions2, #client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").css("display","none"); 
+    $("#update-btn").attr('disabled', true).css({'background-color' : '#dfdfdf', 'cursor' : 'context-menu'});
+    $("#submit-btn").css("display","none");
+    $('.disabled').prop("disabled", true);
+
     $('.search-input').on('keyup', function () {
-        $("#update-btn,#update-btn2").attr('disabled', false);
+        $("#submit-btn").css("display","none");
+        $("#update-btn").attr('disabled', false).css({'background-color' : '#25a69a', 'cursor' : 'pointer'});
+        $("#client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").css("display","none"); 
     });
-    $('#update-btn,#update-btn2').click(function(){
+    
+    $('#update-btn').click(function(){
+        $("#client, #job_owner, #trackingnumber, #need_acceptance, #accept_phone, #trades, #nte, #ok_to_sub, #cam, #cc, #website, #instructions").css("display","none");
+        $("#client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").css("display","block");
+        $(this).css({'background-color' : '#dfdfdf', 'cursor' : 'context-menu'}); 
+        $("#submit-btn").css("display","block");
+    });	
+
+    $('#add-btn').click(function(){
+        $("#submit-btn").css("display","block");
+        $("#update-btn").attr('disabled', true).css({'background-color' : '#dfdfdf', 'cursor' : 'context-menu'});
         CKEDITOR.disableAutoInline = true;
-        $("#client2,#client2 + div").html($('#client').html());
-        $("#job_owner2,#job_owner2 + div").html($('#job_owner').html());
-        $("#trackingnumber2,#trackingnumber2 + div").html($('#trackingnumber').html());
-        $("#need_acceptance2,#need_acceptance2 + div").html($('#need_acceptance').html());
-        $("#accept_phone2,#accept_phone2 + div").html($('#accept_phone').html());
-        $("#trades2,#trades2 + div").html($('#trades').html());
-        $("#nte2,#nte2 + div").html($('#nte').html());
-        $("#ok_to_sub2,#ok_to_sub2 + div").html($('#ok_to_sub').html());
-        $("#cam2,#cam2 + div").html($('#cam').html());
-        $("#cc2,#cc2 + div").html($('#cc').html());
-        $("#website2,#website2 + div").html($('#website').html());
-        $("#instructions2,#instructions2 + div").html($('#instructions').html());
+        $("#client, #job_owner, #trackingnumber, #need_acceptance, #accept_phone, #trades, #nte, #ok_to_sub, #cam, #cc, #website, #instructions").css("display","none");
+        $("#client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").css("display","block");
+        $("#client2, #job_owner2, #trackingnumber2, #need_acceptance2, #accept_phone2, #trades2, #nte2, #ok_to_sub2, #cam2, #cc2, #website2, #instructions2, #client2 + div, #job_owner2 + div, #trackingnumber2 + div, #need_acceptance2 + div, #accept_phone2 + div, #trades2 + div, #nte2 + div, #ok_to_sub2 + div, #cam2 + div, #cc2 + div, #website2 + div, #instructions2 + div").html("");
         CKEDITOR.inline('client2');
         CKEDITOR.inline('jobowner2');
         CKEDITOR.inline('trackingnumber2');
@@ -95,60 +131,7 @@ $(document).ready(function(){
         CKEDITOR.inline('cc2');
         CKEDITOR.inline('website2');
         CKEDITOR.inline('instructions2');
-        $("#client-modal").css('display', 'block');
-        $(".modal-overlay").css('display', 'block');
     });
-    
-    $('#add-btn').click(function(){
-        // $('.disabled').prop("disabled", false);
-        $("#client2,#client2 + div").html("");
-        $("#job_owner2,#job_owner2 + div").html("");
-        $("#trackingnumber2,#trackingnumber2 + div").html("");
-        $("#need_acceptance2,#need_acceptance2 + div").html("");
-        $("#accept_phone2,#accept_phone2 + div").html("");
-        $("#trades2,#trades2 + div").html("");
-        $("#nte2,#nte2 + div").html("");
-        $("#ok_to_sub2,#ok_to_sub2 + div").html("");
-        $("#cam2,#cam2 + div").html("");
-        $("#cc2,#cc2 + div").html("");
-        $("#website2,#website2 + div").html("");
-        $("#instructions2,#instructions2 + div").html("");
-        CKEDITOR.disableAutoInline = true;
-		CKEDITOR.inline('client2');
-		CKEDITOR.inline('jobowner2');
-		CKEDITOR.inline('trackingnumber2');
-		CKEDITOR.inline('acceptance2');
-		CKEDITOR.inline('accept-sr2');
-		CKEDITOR.inline('trades2');
-		CKEDITOR.inline('nte2');
-		CKEDITOR.inline('sub2');
-		CKEDITOR.inline('cam2');
-		CKEDITOR.inline('cc2');
-		CKEDITOR.inline('website2');
-        CKEDITOR.inline('instructions2');
-        
-        // $('.search-input').prop("disabled", true).val("");
-        // $(this).addClass('hide');
-        // $('#update-btn, #label').addClass('hide');
-        // $('#create-btn, #delete-btn').removeClass('hide');
-    });
-    
-    // $('#delete-btn').click(function(){
-    //     $('.disabled').prop("disabled", true).val(" ");
-    //     $('.search-input').prop("disabled", false);
-    //     $(this).addClass('hide');
-    //     $('#trackingnumber').attr('placeholder', this.val);
-    //     $('#create-btn').addClass('hide');
-    //     $('#update-btn, #add-btn, #label').removeClass('hide');
-    //     $('#update-btn').attr("disabled", true);
-    // });		
 });
 
-// CREATE CLIENT - MODAL
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.modal').modal();
-  });
 
